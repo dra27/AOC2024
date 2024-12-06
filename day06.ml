64011312dd09cf037be7dc23603642d002529137
David Allsopp <david.allsopp@metastack.com>
2024-12-06 15:21:20 +0000

    Day 6 - second puzzle

--- day06.ml
+++ day06.ml
@@ -35,7 +35,7 @@ let parse lines =
   lab, Option.get !start
 
 let test =
-  parse @@ String.split_on_char '\n' (String.trim {|
+  String.split_on_char '\n' (String.trim {|
 ....#.....
 .........#
 ..........
@@ -49,8 +49,7 @@ let test =
 |})
 
 let input =
-  In_channel.with_open_text "input-06" @@ fun ic ->
-    parse @@ In_channel.input_lines ic
+  In_channel.with_open_text "input-06" In_channel.input_lines
 
 type deja_vu = N | E | S | W
 
@@ -60,20 +59,24 @@ let turn_right = function
 | S -> W
 | W -> N
 
-let part1 (lab, (x, y)) =
-  let max_x = Array.length lab.(0) in
-  let max_y = Array.length lab in
+let shift max_x max_y x y direction =
+  let shift incr decr value =
+    if direction = incr then
+      succ value
+    else if direction = decr then
+      pred value
+    else
+      value in
+  let x = shift E W x in
+  let y = shift S N y in
+  x, y, (x = -1 || y = -1 || x = max_x || y = max_y)
+
+let part1 input =
+  let (lab, (x, y)) = parse input in
+  let shift = shift (Array.length lab.(0)) (Array.length lab) in
   let rec loop count x y direction =
-    let shift incr decr value =
-      if direction = incr then
-        succ value
-      else if direction = decr then
-        pred value
-      else
-        value in
-    let x' = shift E W x in
-    let y' = shift S N y in
-    if x' = -1 || y' = -1 || x' = max_x || y' = max_y then
+    let x', y', escapes = shift x y direction in
+    if escapes then
       count
     else
       match lab.(y').(x') with
@@ -86,6 +89,54 @@ let part1 (lab, (x, y)) =
           loop (succ count) x' y' direction in
   loop 1 x y N
 
+module PathSet = Set.Make(struct
+  type t = int * int * deja_vu
+  let compare = compare
+end)
+
+let part2 input =
+  let (lab, (x, y)) = parse input in
+  let shift = shift (Array.length lab.(0)) (Array.length lab) in
+  let rec loops path x y direction =
+    let x', y', escapes = shift x y direction in
+    if escapes then
+      false
+    else
+      match lab.(y').(x') with
+      | Obstacle ->
+          loops path x y (turn_right direction)
+      | Empty | Seen ->
+          let this = (x', y', direction) in
+          if PathSet.mem this path then
+            true
+          else
+            loops (PathSet.add this path) x' y' direction in
+  let rec loop count path x y direction =
+    let x', y', escapes = shift x y direction in
+    if escapes then
+      count
+    else
+      match lab.(y').(x') with
+      | Obstacle ->
+          loop count path x y (turn_right direction)
+      | Seen ->
+          let path = PathSet.add (x', y', direction) path in
+          loop count path x' y' direction
+      | Empty ->
+          lab.(y').(x') <- Obstacle;
+          let count =
+            if loops path x y (turn_right direction) then
+              succ count
+            else
+              count in
+          lab.(y').(x') <- Seen;
+          let path = PathSet.add (x', y', direction) path in
+          loop count path x' y' direction in
+  loop 0 (PathSet.add (x, y, N) PathSet.empty) x y N
+
 let () =
   Printf.printf "Day 6; Puzzle 1; test = %d\n\
-                 Day 6; Puzzle 1 = %d\n" (part1 test) (part1 input)
+                 Day 6; Puzzle 1 = %d\n\
+                 Day 6; Puzzle 2; test = %d\n\
+                 Day 6; Puzzle 2 = %d\n" (part1 test) (part1 input)
+                                         (part2 test) (part2 input)
