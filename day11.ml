cb648f2d5a8b9917e37891ea221bb233990f5d89
David Allsopp <david.allsopp@metastack.com>
2024-12-21 19:31:31 +0000

    Day 11 - second puzzle
    As expected! 🤪

--- day11.ml
+++ day11.ml
@@ -1,24 +1,35 @@
-let rec blink = function
-| 0 :: stones ->
-    1 :: blink stones
-| x :: stones when String.length (string_of_int x) land 1 = 0 ->
-    let x = string_of_int x in
-    let l = String.length x in
-    int_of_string (String.sub x 0 (l lsr 1)) ::
-    int_of_string (String.sub x (l lsr 1) (l lsr 1)) ::
-    blink stones
-| x :: stones ->
-    2024 * x :: blink stones
-| [] ->
-    []
+module PairMap = Map.Make(struct type t = int * int let compare = compare end)
 
-let part1 =
-  let rec loop n l =
-    if n = 0 then
-      List.length l
+let rec blink cache x n =
+  try cache, PairMap.find (x, n) cache
+  with Not_found ->
+    let s_x = string_of_int x in
+    let l = String.length s_x in
+    if l land 1 = 0 then
+      if n = 1 then
+        cache, 2
+      else
+        let cache, count1 =
+          let x_upper = int_of_string (String.sub s_x 0 (l lsr 1)) in
+          blink cache x_upper (pred n) in
+        let cache, count2 =
+          let x_lower = int_of_string (String.sub s_x (l lsr 1) (l lsr 1)) in
+          blink cache x_lower (pred n) in
+        let count = count1 + count2 in
+        PairMap.add (x, n) count cache, count
+    else if n = 1 then
+      cache, 1
     else
-      loop (pred n) (blink l) in
-  loop 25
+      blink cache (if x = 0 then 1 else 2024 * x) (pred n)
+
+let part n input =
+  let f (cache, result) x =
+    let cache, this = blink cache x n in
+    cache, this + result in
+  snd @@ List.fold_left f (PairMap.empty, 0) input
+
+let part1 = part 25
+let part2 = part 75
 
 let test =
   List.map int_of_string @@ String.split_on_char ' ' "125 17"
@@ -29,4 +40,7 @@ let input =
 
 let () =
   Printf.printf "Day 11; Puzzle 1; test = %d\n\
-                 Day 11; Puzzle 1 = %d\n" (part1 test) (part1 input)
+                 Day 11; Puzzle 1 = %d\n\
+                 Day 11; Puzzle 2; test = %d\n\
+                 Day 11; Puzzle 2 = %d\n" (part1 test) (part1 input)
+                                          (part2 test) (part2 input)
